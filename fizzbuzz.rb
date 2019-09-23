f8baffda5989e262b8ddf398ac6382df5bb9f28d
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-23 01:11:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elsif int % 5 == 0
+    "Buzz"
   end
 end
 
-def fizzbuzz(int)
-  if int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go fizz
-  end
-end
+
  
  
